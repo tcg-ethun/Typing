c2edf2c2a3de1f05940bc283851eda506a4eb9c3
Esan <ethuntcg@gmail.com>
2025-03-29 20:35:49 +0600

    Update

---
+++ script.js
@@ -0,0 +1,13 @@
+
+
+let srt = "Hello Ethun , How are You ?";
+console.log(srt.indexOf("You"));
+
+let i = 0
+for( ;  ;  ){
+    console.log(i)
+    i++;
+    if(i === 10){
+        break;
+    }
+}

fc2b4a73e36d96bf93517b92cbbebfe012630078
Esan <ethuntcg@gmail.com>
2025-03-30 23:32:01 +0600

    Update

---
+++ ok.js
@@ -0,0 +1,17 @@
+
+document.getElementById("submit");
+
+function Perform(){
+    let userValue = document.getElementById("inputValue").value;
+    document.getElementById("output").textContent = userValue;
+
+}
+
+let country = `Bangladesh`;
+let a = 17;
+let b = 1;
+let x = 0.2;
+let y = 0.3;
+let sum = `The Sum is ${(x)+ (y)}`
+
+console.log(sum);
